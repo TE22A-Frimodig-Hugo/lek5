b891920aa5493d50778b22b00d056ed54fb97f05
Hugo Frimodig <Hugo.Frimodig@elev.ga.ntig.se>
2023-09-27 12:17:59 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       int[] fält1 = {8,4,6,3,9};
+       int[] fält2 = {18,14,16,13,19};
+       System.out.println(fält1.length);
+       System.out.println(fält2.length);
+       double tal1 = 30;
+       tal1 = 5;
+       double tal2 = 80;
+        tal2 = 5; 
+       double delat = tal1/tal2;
+       double summa = tal1+tal2;
+       System.out.println(tal1+"/"+tal2+"="+delat);
+       System.out.println(tal1+"+"+tal2+"="+summa);
+
+    }
+}
